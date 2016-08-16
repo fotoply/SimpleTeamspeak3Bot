bb90ea0b867a659bcf65cfeaf9154070e61fed38
fotoply <fotoply@gmail.com>
2016-08-16 03:13:44 +0200

    Started implementing settings command

--- src/main/java/commands/SettingsCommand.java
+++ src/main/java/commands/SettingsCommand.java
@@ -2,10 +2,26 @@ package commands;
 
 import com.github.theholywaffle.teamspeak3.TS3Api;
 import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
+import control.SettingsHandler;
 
 public class SettingsCommand implements ICommand {
+    SettingsHandler settingsHandler;
+
     @Override
     public void run(TS3Api api, String[] args, TextMessageEvent event) {
+        settingsHandler = SettingsHandler.getInstance();
+
+        if(settingsHandler.isSettingValid(args[1])) {
+            if(args[2].equalsIgnoreCase("get")) {
+
+            } else if(args[2].equalsIgnoreCase("set")) {
+
+            } else {
+                api.sendPrivateMessage(event.getInvokerId(), "Please either use set or get");
+            }
+        } else {
+            api.sendPrivateMessage(event.getInvokerId(), "No such setting exists");
+        }
         //TODO Implement a functional settings command.
     }
 
