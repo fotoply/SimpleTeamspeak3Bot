24c3603f872e62c901cfb815ec7e4fa4bc4f14a1
fotoply <fotoply@gmail.com>
2016-08-16 19:11:57 +0200

    Revamped settingValidator to always take a string.

--- src/main/java/control/Setting.java
+++ src/main/java/control/Setting.java
@@ -2,14 +2,30 @@ package control;
 
 public class Setting {
     private SettingValidator validator;
-    private String value;
+    private String defaultValue;
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public boolean isValueValid(String value) {
+        return validator.isValidValue(value);
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public void setValidator(SettingValidator validator) {
+        this.validator = validator;
+    }
 
     /*TODO Implement setting class and make it have the following:
-    The ability to have an unrestricted setting, where the value can be any user-chosen value
-    The ability to have a semi-restricted numeric value, where the event/command pre-defines a range
-    The ability to have a semi-restricted string value where the event/command pre-defines this
-    The ability to have a restricted string value where the event/command pre-defines all values possible
+    The ability to have an unrestricted setting, where the values can be any programmer-chosen defaultValue
+    The ability to have a semi-restricted numeric defaultValue, where the event/command pre-defines a range
+    The ability to have a semi-restricted string defaultValue where the event/command pre-defines this
+    The ability to have a restricted string defaultValue where the event/command pre-defines all values possible
 
-    It should also contain a way to register a default value.
+    It should also contain a way to register a default defaultValue.
     */
 }
--- src/main/java/control/SettingValidator.java
+++ src/main/java/control/SettingValidator.java
@@ -1,5 +1,5 @@
 package control;
 
-public abstract class SettingValidator {
-    public abstract boolean validateValue(Object value);
+public interface SettingValidator {
+    boolean isValidValue(String value);
 }
