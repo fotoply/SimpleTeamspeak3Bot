7112ade629ec800e592a4a3db12025bb8e2a37c7
fotoply <fotoply@gmail.com>
2016-08-16 23:18:46 +0200

    Added range setting validator for integers

---
+++ src/main/java/control/settingvalidators/IntegerRangeSettingValidator.java
@@ -0,0 +1,32 @@
+package control.settingvalidators;
+
+/**
+ * Allows any integer within a pre-specified range
+ * This range is inclusive on both ends
+ */
+public class IntegerRangeSettingValidator extends AbstractSettingValidator {
+    int upperLimit;
+    int lowerLimit;
+
+    public IntegerRangeSettingValidator(int upperLimit, int lowerLimit) {
+        this.upperLimit = upperLimit;
+        this.lowerLimit = lowerLimit;
+    }
+
+    @Override
+    public boolean isValidValue(String value) {
+        int numberValue;
+        try {
+            numberValue = Integer.valueOf(value);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        if(numberValue < lowerLimit) {
+            return false;
+        }
+        if (numberValue > upperLimit) {
+            return false;
+        }
+        return true;
+    }
+}
