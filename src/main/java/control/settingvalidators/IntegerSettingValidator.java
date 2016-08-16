d5b43c59d54cbf5a37421dd018ecc149d8095862
fotoply <fotoply@gmail.com>
2016-08-16 22:36:23 +0200

    Added an integer validator

---
+++ src/main/java/control/settingvalidators/IntegerSettingValidator.java
@@ -0,0 +1,16 @@
+package control.settingvalidators;
+
+/**
+ * Allows any integer value to be used
+ */
+public class IntegerSettingValidator extends AbstractSettingValidator {
+    @Override
+    public boolean isValidValue(String value) {
+        try {
+            Integer.valueOf(value);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+}
