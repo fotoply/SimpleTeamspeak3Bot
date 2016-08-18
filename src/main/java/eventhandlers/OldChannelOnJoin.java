bbab429c0c17e0878d3f2f1180485dd9480a5dfc
fotoply <fotoply@gmail.com>
2016-08-18 22:36:07 +0200

    Fix incorrect call to get the unique ID of the joining user

--- src/main/java/eventhandlers/OldChannelOnJoin.java
+++ src/main/java/eventhandlers/OldChannelOnJoin.java
@@ -25,7 +25,7 @@ public class OldChannelOnJoin implements IOnJoinEvent, IOnMovedEvent, IOnBotShut
     @Override
     public void onJoin(TS3Api api, ClientJoinEvent joinEvent) {
         if (lastChannel.getOrDefault(joinEvent.getUniqueClientIdentifier(), 1) != 1) {
-            String currentSetting = SettingsHandler.getInstance().getSettingOrDefault(joinEvent.getInvokerUniqueId(), SETTING_NAME);
+            String currentSetting = SettingsHandler.getInstance().getSettingOrDefault(joinEvent.getUniqueClientIdentifier(), SETTING_NAME);
             if(currentSetting.equalsIgnoreCase("on") || currentSetting.equalsIgnoreCase("enable")) {
                 api.moveClient(joinEvent.getClientId(), lastChannel.get(joinEvent.getUniqueClientIdentifier()));
             }
