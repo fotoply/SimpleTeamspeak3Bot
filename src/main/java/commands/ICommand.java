8724314ebf47ef7bb835f19eec2cde0f5caa3e55
fotoply <fotoply@gmail.com>
2016-08-20 05:03:47 +0200

    More JavaDocs added

--- src/main/java/commands/ICommand.java
+++ src/main/java/commands/ICommand.java
@@ -4,13 +4,35 @@ import com.github.theholywaffle.teamspeak3.TS3Api;
 import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
 
 public interface ICommand {
+    /**
+     * Is called when the associated basis command is sent to the server.
+     * @param api
+     * @param args
+     * @param event
+     */
     void run(TS3Api api, String[] args, TextMessageEvent event);
 
+    /**
+     * Gets a string representing the command, including any prefix
+     * @return
+     */
     String getCommand();
 
+    /**
+     * Should return a short descriptive String for the command
+     * @return
+     */
     String getHelpText();
 
+    /**
+     * Should return a short command-structure String
+     * @return
+     */
     String getExtendedHelpText();
 
+    /**
+     * Should return the power required for executing this command
+     * @return
+     */
     int getPowerRequired();
 }
