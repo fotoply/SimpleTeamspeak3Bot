354ca537a0baff1078ccc7e12da48346c336b2b2
fotoply <fotoply@gmail.com>
2016-08-21 04:28:25 +0200

    Added some TODO's

---
+++ src/main/java/commands/LastSeenCommand.java
@@ -0,0 +1,5 @@
+package commands;
+
+public class LastSeenCommand {
+    //TODO Make this command give back the latest seen for a given username, with UID's attached for usernames seen over multiple
+}
---
+++ src/main/java/eventhandlers/AutoAFKMover.java
@@ -0,0 +1,5 @@
+package eventhandlers;
+
+public class AutoAFKMover {
+    //TODO Implement this so that it automatically moves people when they have been inactive for too long and moves them back when they become active
+}
