5b2c8b20a87ed87e707fc8c562e8fd0d0b92dbca
fotoply <fotoply@gmail.com>
2016-08-24 20:35:15 +0200

    Now should save information whenever a user joins, currently no way to display it.

--- src/main/java/commands/LastSeenCommand.java
+++ src/main/java/commands/LastSeenCommand.java
@@ -1,11 +1,31 @@
 package commands;
 
 import com.github.theholywaffle.teamspeak3.TS3Api;
+import com.github.theholywaffle.teamspeak3.api.event.ClientJoinEvent;
 import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
+import control.utils.MapPersistence;
 import events.IOnBotInitializedEvent;
 import events.IOnBotShutdownEvent;
+import events.IOnJoinEvent;
 
-public class LastSeenCommand implements ICommand, IOnBotInitializedEvent, IOnBotShutdownEvent {
+import java.io.File;
+import java.util.HashMap;
+
+public class LastSeenCommand implements ICommand, IOnBotInitializedEvent, IOnBotShutdownEvent, IOnJoinEvent {
+    private static final String SAVE_PATH = "data/lastseen.data";
+    private static HashMap<String, HashMap<String, String>> lastseenInfoMap;
+    private static LastSeenCommand instance;
+    private LastSeenCommand() {
+        lastseenInfoMap = new HashMap<>();
+    }
+
+    public static LastSeenCommand getInstance() {
+        if(instance == null) {
+            instance = new LastSeenCommand();
+        }
+        return instance;
+    }
+
     @Override
     public void run(TS3Api api, String[] args, TextMessageEvent event) {
 
@@ -33,12 +53,23 @@ public class LastSeenCommand implements ICommand, IOnBotInitializedEvent, IOnBot
 
     @Override
     public void onBotInitialized(TS3Api api) {
-
+        File dataFile = new File(SAVE_PATH);
+        if (dataFile.exists()) {
+            lastseenInfoMap.putAll(MapPersistence.readStringNestedMap(dataFile));
+        }
     }
 
     @Override
     public void onBotShutdown(TS3Api api, int shutdownCode) {
+        MapPersistence.saveGenericMap(new File(SAVE_PATH), lastseenInfoMap);
+    }
 
+    @Override
+    public void onJoin(TS3Api api, ClientJoinEvent joinEvent) {
+        HashMap<String, String> info = new HashMap<>();
+        info.put("username", joinEvent.getClientNickname());
+        info.put("lastseen", String.valueOf(System.currentTimeMillis()));
+        lastseenInfoMap.put(joinEvent.getUniqueClientIdentifier(), info);
     }
     //TODO Make this command give back the latest seen for a given username, with UID's attached for usernames seen over multiple
 }
