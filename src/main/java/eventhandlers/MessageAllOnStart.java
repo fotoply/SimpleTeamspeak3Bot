dafad951f73f4067596f3d4a6e45e109f47997c0
fotoply <fotoply@gmail.com>
2016-09-09 20:05:33 +0200

    Now registers a setting for itself so that the message can be disabled

--- src/main/java/eventhandlers/MessageAllOnStart.java
+++ src/main/java/eventhandlers/MessageAllOnStart.java
@@ -3,22 +3,48 @@ package eventhandlers;
 import com.github.theholywaffle.teamspeak3.TS3Api;
 import com.github.theholywaffle.teamspeak3.api.event.ClientJoinEvent;
 import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
+import control.Setting;
+import control.SettingsHandler;
+import control.settingvalidators.PredefinedSettingValidator;
 import events.IOnBotInitializedEvent;
 import events.IOnJoinEvent;
 
 import java.util.ArrayList;
 
 public class MessageAllOnStart implements IOnBotInitializedEvent, IOnJoinEvent {
+
+    public static final String SETTING_NAME = "pm_on_join";
+
     @Override
     public void onBotInitialized(TS3Api api) {
+        PredefinedSettingValidator validator = new PredefinedSettingValidator();
+        validator.addAllowedOption("on");
+        validator.addAllowedOption("off");
+        validator.addAllowedOption("enable");
+        validator.addAllowedOption("disable");
+        SettingsHandler.getInstance().registerSetting(SETTING_NAME, new Setting(validator, "on"));
+
         ArrayList<Client> clients = (ArrayList<Client>) api.getClients();
         for (Client client : clients) {
-            api.sendPrivateMessage(client.getId(), "Welcome to the server, type !help for commands");
+            if(isSettingEnabled(client.getUniqueIdentifier())) {
+                sendWelcomeMessage(api, client.getId());
+            }
         }
+    }
+
+    private boolean isSettingEnabled(String UID) {
+        return SettingsHandler.getInstance().getSettingOrDefault(UID, SETTING_NAME).equalsIgnoreCase("on") ||
+               SettingsHandler.getInstance().getSettingOrDefault(UID, SETTING_NAME).equalsIgnoreCase("enable");
+    }
+
+    private void sendWelcomeMessage(TS3Api api, int id) {
+        api.sendPrivateMessage(id, "Welcome to the server, type !help for commands");
     }
 
     @Override
     public void onJoin(TS3Api api, ClientJoinEvent joinEvent) {
-        api.sendPrivateMessage(joinEvent.getClientId(), "Welcome to the server, type !help for commands");
+        if(isSettingEnabled(joinEvent.getUniqueClientIdentifier())) {
+            sendWelcomeMessage(api, joinEvent.getClientId());
+        }
     }
 }
